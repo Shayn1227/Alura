8c1aef76df65bd4adfac557a21e84aeca1854ec7
Shayn1227 <antonyjamarques@gmail.com>
2023-03-27 15:52:50 -0300

    Projeto login completo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,24 @@ module.exports = {
         azul: {
           claro: '#C5DFFF',
           escuro: '#061E3C',
-          hover: '#10557B0'
+          hover: '#1057B0'
         },
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif']
+      },
+      keyframes: {
+        sino_kf: {
+          '0%, 100%': {
+            transform: 'rotate(-10deg)'
+          },
+          '50%': {
+            transform: 'rotate(10deg)'
+          }
+        }
+      },
+      animation: {
+        sino: 'sino_kf 0.31s ease-in-out infinite'
       }
     }
   },
